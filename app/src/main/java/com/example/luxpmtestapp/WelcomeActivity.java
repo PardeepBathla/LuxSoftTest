169d7c465550ad7ace6dd7222f7bfaed5806ea0f
mgupta@mind-roots.com <mgupta@mind-roots.com>
2021-06-12 15:39:05 +0530

    first commit

---
+++ app/src/main/java/com/example/luxpmtestapp/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package com.example.luxpmtestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.luxpmtestapp.databinding.ActivityWelcomeBinding;
+
+public class WelcomeActivity extends AppCompatActivity {
+    ActivityWelcomeBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
+
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToRegistrationScreen();
+            }
+        });
+
+    }
+
+    private void goToRegistrationScreen(){
+        startActivity(new Intent(WelcomeActivity.this, RegistrationActivity.class));
+    }
+}
